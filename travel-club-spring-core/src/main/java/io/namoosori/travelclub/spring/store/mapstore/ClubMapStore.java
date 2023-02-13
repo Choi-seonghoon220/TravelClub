f22e5f5ad76f3ce79d165f150457a62c37621548
CHOI_SeongHoon <ilbtjesus220@gmail.com>
2023-02-13 21:26:02 +0900

    1

---
+++ travel-club-spring-core/src/main/java/io/namoosori/travelclub/spring/store/mapstore/ClubMapStore.java
@@ -0,0 +1,51 @@
+package io.namoosori.travelclub.spring.store.mapstore;
+
+import io.namoosori.travelclub.spring.aggregate.club.TravelClub;
+import io.namoosori.travelclub.spring.store.ClubStore;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ClubMapStore implements ClubStore {
+
+    private Map<String, TravelClub> clubMap;
+
+    public ClubMapStore(){
+        this.clubMap = new LinkedHashMap<>();
+    }
+
+    @Override
+    public String create(TravelClub club) {
+        clubMap.put(club.getId(), club);
+        return club.getId();
+    }
+
+    @Override
+    public TravelClub retrieve(String clubId) {
+        return clubMap.get(clubId);
+    }
+
+    @Override
+    public List<TravelClub> retrieveByName(String name) {
+        return clubMap.values().stream()
+                .filter(club -> club.getName().equals(name))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void update(TravelClub club) {
+
+    }
+
+    @Override
+    public void delete(String clubId) {
+
+    }
+
+    @Override
+    public boolean exists(String clubId) {
+        return false;
+    }
+}
